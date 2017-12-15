07afc1739e890c6a776d86799fa72fd2b35bfe02
XinTan <838524739@qq.com>
2017-12-15 11:05:57 +0800

    ArchivesSystem start!

---
+++ ArchivesSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArchivesSystem.Startup))]
+namespace ArchivesSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
